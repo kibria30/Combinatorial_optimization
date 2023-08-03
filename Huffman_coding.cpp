b14bacbe9a4da7ed9f9d5a53d885855d0822079a
kibria30 <bsse1430@iit.du.ac.bd>
2023-08-03 22:58:53 +0600

    Huffman tree done

--- Huffman_coding.cpp
+++ Huffman_coding.cpp
@@ -11,49 +11,46 @@ struct node{
 struct node nodes[10000];
 struct node data[10000];
 
-struct node extract_min(int last){
+struct node* extract_min(int last){
     struct node *min = NULL ;
     struct node *temp = (struct node*)malloc(sizeof(struct node));
     temp->ch= '\0';
     temp->freq = 100000;
     temp->left = NULL;
     temp->right = NULL; 
-    *min = *temp;
+    min = temp;
     for(int i=0; i<=last; i++){
         if(!nodes[i].isdone && nodes[i].freq < min->freq){
-            *min = nodes[i];
-            //index = i;
+            min = &nodes[i];
         }
     }
     min->isdone = true;
-    return *min;
+    return min;
 }
 
-struct node huffman(int size, struct node *root){
+struct node* huffman(int size){
     int ptr = size-1;
+    struct node *x, *y;
     for(int i=1; i<size; i++){   //binary tree er edge number = (vertex - 1);
-        struct node temp, x, y;
         x = extract_min(ptr);
         y = extract_min(ptr);
-        temp.ch = '\0';
-        temp.left = &x;
-        temp.right = &y;
-        temp.freq = x.freq + y.freq;
         ptr++;
-        nodes[ptr] = temp;
-        cout<<x.freq<<" "<<y.freq<<endl;
-        cout<<"store "<<temp.freq<<"  at ptr "<<ptr<<endl;
+        nodes[ptr].ch = '\0';
+        nodes[ptr].freq = x->freq + y->freq;
+        nodes[ptr].isdone = false;
+        nodes[ptr].left = x;
+        nodes[ptr].right = y;
+        cout<<x->freq<<" "<<y->freq<<endl;
+        cout<<"store "<<nodes[ptr].freq<<"  at ptr "<<ptr<<endl;
     }
-    //struct node temp2 = extract_min(ptr);
-    *root = extract_min(ptr);
-    //return root;
+    return extract_min(ptr);
 };
 
 void preorder(struct node *root){
-    if(root){
+    if(!root){
         return;
     }
-    cout<<root->freq;
+    cout<<root->freq<<" ";
     preorder(root->left);
     preorder(root->right);
 }
@@ -72,12 +69,9 @@ int main(){
     // for(int i = 0; i<n; i++){
     //     cout<<nodes[i].ch<<" "<<nodes[i].freq<<endl;
     // }
-    struct node root;
-    huffman(n, &root);
-    cout<<"from root "<<root.right->freq<<endl;
-    preorder(&root);
-    // for(int i = 0; i<10; i++){
-    //     cout<<nodes[i].ch<<" "<<nodes[i].freq<<endl;
-    // }
+    struct node *root;
+    root = huffman(n);
+    //cout<<"from root "<<root.right->freq<<endl;
+    preorder(root);
     return 0;
 }
