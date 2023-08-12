71c44ef1fa945512d006113f21220239bec5f317
kibria30 <bsse1430@iit.du.ac.bd>
2023-08-12 20:23:24 +0600

    rod cutting done

--- rodCutting.cpp
+++ rodCutting.cpp
@@ -5,8 +5,10 @@ struct rod{
     int length;
     int price;
     int maxProfit;
+    pair<int, int> lengths;
 }cutLength[1000];
 int numberOfInput, totalLength;
+int totalcut = 0;
 
 void readInput(){
     cin>>numberOfInput>>totalLength;
@@ -19,22 +21,35 @@ void readInput(){
 void printResult(){
     for(int i=1; i<=numberOfInput; i++){
         cout<<cutLength[i].length<<" "<<cutLength[i].price<<" "<<cutLength[i].maxProfit<<endl;
+        cout<<"pairs :"<< cutLength[i].lengths.first<<" "<<cutLength[i].lengths.second<<endl; 
     }
     cout<<"------------------------------------------------------------------"<<endl;
 }
 
 int calcMaxProfit(int i){
-    if(cutLength[i].maxProfit >= 0){
+    cout<<"length: "<<i<<endl;
+    if(cutLength[i].maxProfit > 0){
         return cutLength[i].maxProfit;
     } 
     else if( i==0 ){
         return 0;
     }
     else{
-        int profit = -1;
-        for(int j=1; j<=i; j
-            profit = max(profit, cutLength[j].price + calcMaxProfit(i-j));
+        int profit = -1, length1, length2;
+        for(int j=1; j<=i; j++){
+            if(cutLength[j].price == 0) continue;
+            int calculatedprofit = cutLength[j].price + calcMaxProfit(i-j);
+            if(calculatedprofit > profit){
+                length1 = j;
+                length2 = i - j;
+                profit = calculatedprofit;
+            }
+            //profit = max(profit, cutLength[j].price + calcMaxProfit(i-j));
         }
+        cutLength[i].lengths.first = length1; 
+        cutLength[i].lengths.second = length2; 
+        //lengths.push_back(length);
+        //cout<<"for maxprofit for "<<i<< " take length: "<< length
         return profit;
     } 
 }
@@ -45,13 +60,33 @@ void cutRod(){
     }
 }
 
+void printcutlength(int lentgh){
+    int length1 = cutLength[lentgh].lengths.first;
+    int length2 = cutLength[lentgh].lengths.second;
+    if( length1 == 0 || length2 == 0){
+        totalcut++;
+        if(length1){
+            cout<<length1<<" ";
+        }
+        else cout<< length2<<" ";
+    }
+    else{
+        printcutlength(length1);
+        printcutlength(length2);
+    }
+}
+
 int main(){
     
     freopen("rodCutting_input.txt", "r", stdin);
     readInput();
-    //printResult();
+    printResult();
     cutRod();
-    //printResult();
-    //cout<<cutLength[].price;
+    printResult();
     cout<<"Maxprofit for length "<<totalLength<<" is : "<<cutLength[totalLength].maxProfit<<endl;
+    cout<<"take this lengths: ";
+
+    printcutlength(totalLength);
+    cout<<endl;
+    cout<<"total cut: "<<totalcut<<endl;
 }
