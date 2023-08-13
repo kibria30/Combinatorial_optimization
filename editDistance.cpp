0c662918d4b8c9f2e5eb7b01cc343ca60f1568e4
kibria30 <bsse1430@iit.du.ac.bd>
2023-08-14 00:48:52 +0600

    edit distance done

--- editDistance.cpp
+++ editDistance.cpp
@@ -1,101 +1,81 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#define leftarrow -100
+#define uparrow -101
+#define diagarrow -102
+#define empdiarrow -103
+#define MAX 1000
 using namespace std;
 
-char up = 'u';
-char leftr = 'l';
-char diagonal = 'd';
-char none = 'n';
-
-char a[] = "aloo";
-char b[] = "potol";
-
-struct node{
-    char arrow;
+struct cell{
+    int count;
     int operation;
-}matrix[1000][1000];
+}matrix[MAX][MAX];
 
-int findMin(int x, int y, int z){
-    if (x <= y && x <= z)
-        return x;
-    else if (y <= x && y <= z)
-        return y;
-    else
-        return z;
-}
+char str1[100], str2[100];
 
-int findDistance(char a[], char b[]){
-    int len_a = strlen(a);
-    int len_b = strlen(b);
-
-    for (int i = 0; i <= len_a; i++){
-        matrix[i][0].arrow = up;
-        matrix[i][0].operation = i;
+void editDistance(int m, int n){
+    matrix[0][0].count = 0;
+    for(int i=1; i<=m ; i++){
+        matrix[i][0].count = i;
+        matrix[i][0].operation = uparrow;
     }
 
-    for (int j = 0; j <= len_b; j++){
-        matrix[0][j].arrow = leftr;
-        matrix[0][j].operation = j;
+    for(int j=1; j<=n; j++){
+        matrix[0][j].count = j;
+        matrix[0][j].operation = leftarrow;
     }
 
-    for (int i = 1; i <= len_a; i++){
-        for (int j = 1; j <= len_b; j++){
-            if (a[i - 1] == b[j - 1]){
-                matrix[i][j].arrow = none;
-                matrix[i][j].operation = matrix[i-1][j-1].operation;
+    for(int i=1; i<=m; i++){
+        for(int j=1; j<=n; j++){
+            if(str1[i-1] == str2[j-1]){     // matrix from 1,1 but string from 0 index
+                matrix[i][j].count = matrix[i-1][j-1].count;
+                matrix[i][j].operation = empdiarrow;
+            }
+            else if(matrix[i-1][j-1].count <= matrix[i-1][j].count && matrix[i-1][j-1].count <= matrix[i][j-1].count){
+                matrix[i][j].count = matrix[i-1][j-1].count + 1;
+                matrix[i][j].operation = diagarrow;
+            }
+            else if(matrix[i-1][j].count <= matrix[i-1][j-1].count && matrix[i-1][j].count <= matrix[i][j-1].count){
+                matrix[i][j].count = matrix[i-1][j].count + 1;
+                matrix[i][j].operation = uparrow;
             }
             else{
-                int min = findMin(matrix[i - 1][j - 1].operation, matrix[i - 1][j].operation, matrix[i][j - 1].operation);
-                if(min == matrix[i - 1][j - 1].operation){
-                    matrix[i][j].arrow = diagonal;
-                }
-                else if(min == matrix[i - 1][j].operation){
-                    matrix[i][j].arrow = up;
-                }
-                else{
-                    matrix[i][j].arrow = leftr;
-                }
-                
-                matrix[i][j].operation = 1 + min;
+                matrix[i][j].count = matrix[i][j-1].count + 1;
+                matrix[i][j].operation = leftarrow;
             }
         }
     }
-
-    int answer = matrix[len_a][len_b].operation;
-    return answer;
 }
 
-void printPath(int x, int y){
-    if( x<0 || y<0){
-        return;
+void printOperations(int m, int n){
+    if(m==0 && n==0) return;
+    if(matrix[m][n].operation == uparrow){
+        printOperations(m-1, n);
+        cout<<"Delete: "<<str1[m-1]<<endl;     // matrix from 1,1 but string from 0 index
     }
-    if(matrix[x][y].arrow == diagonal){
-        printPath(x-1, y-1);
+    else if(matrix[m][n].operation == leftarrow){
+        printOperations(m, n-1);
+        cout<<"Insert: "<<str2[n-1]<<endl;      // matrix from 1,1 but string from 0 index
     }
-    else if(matrix[x][y].arrow == up){
-        printPath(x-1, y);
+    else if(matrix[m][n].operation == diagarrow){
+        printOperations(m-1, n-1);
+        cout<<"Replace: "<<str1[m-1]<<" by "<<str2[n-1]<<endl;   // matrix from 1,1 but string from 0 index
     }
     else{
-        printPath(x, y-1);
+        printOperations(m-1, n-1);
+        cout<<"No operation needed."<<endl;
     }
-
-    if(matrix[x][y].arrow == none){
-        cout<<"No oeration needed"<<endl;
-    }
-    else if(matrix[x][y].arrow == leftr){
-        cout<<"insert"<<endl;
-    }
-    else if(matrix[x][y].arrow == up){
-        cout<<"delete"<<endl;
-    }
-    else cout<<"replace"<<endl;
-    //cout<<matrix[a][b].arrow<<"->";
 }
 
 int main(){
-    
 
-    cout << "Total operations: " << findDistance(a, b)<<endl;
-    printPath(strlen(a), strlen(b));
-    cout<<endl;
-    return 0;
+    freopen("ED_input.txt", "r", stdin);
+    cin>>str1>>str2;
+    int m = strlen(str1);
+    int n = strlen(str2);
+
+    editDistance(m, n);
+    cout<<"Total operations needed: "<<matrix[m][n].count<<endl;
+    printOperations(m, n);
+
 }
